dcb42df0cb5d0c03f494501be67fc3081dd299be
Daphne <daqu2831@colorado.edu>
2022-10-12 21:45:26 +0000

    Setting up utils.py

---
+++ survey_analysis/utils.py
@@ -0,0 +1,18 @@
+''' Functions to aid in the survey analysis
+
+    * cramersV - Finds the Cramers-V correlation coefficient
+
+'''
+
+def cramersV():
+    '''
+    Calculate the Cramers-V correlation coefficient for categorical data
+    
+    Parameters
+    ----------
+    
+    Returns
+    -------
+    
+    '''
+    return -1
