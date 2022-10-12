438e38e09c93291238c711601534ffcaf04d7a72
Daphne <daqu2831@colorado.edu>
2022-10-12 22:36:32 +0000

    Setting up argparse

---
+++ survey_analysis/correlation.py
@@ -0,0 +1,52 @@
+# Goal: given a csv file of survey responses, find the correlation 
+# that that demographic has to taking calc 2 in the future
+
+# Inputs:
+#   - The dataset to use
+#   - The given demographic: 
+#      - Select multiple options from:
+#          - Gender identity
+#          - Race/ethnicity
+#          - Highest math course
+#          - Field 
+#      - Default is all options
+
+# Simplify long names in survey:
+# Gender identity: [cis-woman, cis-man, trans-woman, trans-man, non-bin, agender, intersex, g-fluid, other]
+# Race/ethnicity: [white, black, indig-A-AK, asian, pac-isl, hisp-latx, 2+, other]
+# Highest math: [alg, pre-calc, calcAB, calcBC, other]
+# field: [bio, chem, phys, math, comp-sci, eng, econ, envs, bus, educ, perf-arts, vis_arts, soc-sci, eng-comm
+#         phil, mod-lan, med, ear-spc, law, soc-serv]
+
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(
+                description='Choose a search method to find a value in a list',
+                prog='correlation.py')
+
+    parser.add_argument('--file_name',
+                        type=str,
+                        help='Name of the file with survey data',
+                        required=True)
+    parser.add_argument('--gender_identity',
+                        type=list,
+                        help='The list of gender identities to examine',
+                        required=False)
+    parser.add_argument('--race_ethnicity',
+                        type=list,
+                        help='The list of race/ethnicities to examine',
+                        required=False)
+    parser.add_argument('--highest_math',
+                        type=list,
+                        help='The list of previous math experience to examine',
+                        required=False)
+    parser.add_argument('--field',
+                        type=list,
+                        help='The list of intended fields to examine',
+                        required=False)
+
+    args = parser.parse_args()
+    
+if __name__ == '__main__':
+    main()
