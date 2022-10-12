04ca95b9e63bf5dd587cf1f3cb84badd5310b694
Daphne <daqu2831@colorado.edu>
2022-10-12 21:50:31 +0000

    Setting up test_utils.py

---
+++ survey_analysis/test_utils.py
@@ -0,0 +1,22 @@
+import unittest
+import utils
+import random
+import numpy as np
+
+
+class TestUtils(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        return -1
+
+    @classmethod
+    def tearDownClass(cls):
+        return -1
+
+    def test_cramersV(self):
+        return -1
+
+
+if __name__ == '__main__':
+    unittest.main()
