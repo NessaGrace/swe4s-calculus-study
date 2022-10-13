98f1e87942be0f58f77a2c003b19522c3f3405e7
marisapetrusky <marisa.petrusky@colorado.edu>
2022-10-13 16:32:16 +0000

    Check if probabilities add to 1, but bug in args.param_delim

--- simulate_data/create_data.py
+++ simulate_data/create_data.py
@@ -3,6 +3,7 @@ import csv
 import argparse
 import random
 import pdb
+import sys
 
 
 def main():
@@ -10,7 +11,7 @@ def main():
     parser = argparse.ArgumentParser()
     # File name to write simulated data to (.csv only, 10/9/22)
     parser.add_argument('--write_file_name', type=str, required=True)
-    # File name to take input parameters from (.txt tab delimited recommended)
+    # File name to take input parameters from (.txt tab delimited only)
     parser.add_argument('--param_file_name', type=str, required=True)
     parser.add_argument('--param_delim', type=str, required=True)
     # Statistically significant sample size: 200
@@ -22,18 +23,27 @@ def main():
     question_header = []
     answer_header = []
     probability = []
+    tolerance = 1e-6
 
     # Go through parameter file, fill lists
     with open(args.param_file_name, 'r') as f_read:
         iter = 0
         for row in f_read:
-            list = row.split('\t')
+            info = row.rstrip().split('\t')
             if (iter % 2 == 0):
-                question_header.append(list[0])
-                answer_header.append(list[1:])
+                question_header.append(info[0])
+                answer_header.append(info[1:])
             else:
-                probability.append(list[1:])  # TODO Prob add to 1
+                probability.append(info[1:])
             iter += 1
+        for iQ in range(0, len(question_header)):
+            sum = 0
+            for iAns in range(0, len(probability[iQ])):
+                sum += float(probability[iQ][iAns])
+            if abs(sum - 1) > tolerance:  # Probabilities should approx add to 1
+                print('Warning: Probabilities of Question ' + str(iQ) + ' does not '
+                      + 'add to 1. Quitting...')
+                sys.exit(1)
 
     with open(args.write_file_name, 'w', newline='') as f_write:
         writer = csv.writer(f_write)
