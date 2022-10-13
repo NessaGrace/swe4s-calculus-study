a6d3aad0dd4b2bfacb762a7d0dfc4ec3e01c6248
Daphne <daqu2831@colorado.edu>
2022-10-13 22:02:11 +0000

    Fixing issue with test_utils

--- survey_analysis/test_utils.py
+++ survey_analysis/test_utils.py
@@ -7,23 +7,17 @@ import pandas as pd
 
 class TestUtils(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        # set up sample dataframe to work with
-        data = [['A', 10], ['B', 15], ['C', 14], ['D', 15]]
-        cls.df = pd.DataFrame(data, columns=['Names', 'Counts'])
-
-    @classmethod
-    def tearDownClass(cls):
-        df = None
-
     def test_cramersV(self):
         return -1
     
     def test_filter_data(self):
+        # Set up data
+        data = [['A', 10], ['B', 15], ['C', 14], ['D', 15]]
+        df = pd.DataFrame(data, columns=['Names', 'Counts'])
+        
         # Positive test
         target_df = pd.DataFrame([['A', 10], ['B', 15], ['D', 15]], columns=['Names', 'Counts'])
-        self.assertEqual(utils.filter_data(cls.df, 'Counts', [15, 10]), target_df)
+        self.assertEqual(utils.filter_data(df, 'Counts', [15, 10]), target_df)
 
 
 if __name__ == '__main__':
