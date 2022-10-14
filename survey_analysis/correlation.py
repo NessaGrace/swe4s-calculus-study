1054d70dd3b6f79ec7207d8d77563a9d21f08fcc
Daphne <daqu2831@colorado.edu>
2022-10-14 17:04:49 +0000

    implementing filter_data

--- survey_analysis/correlation.py
+++ survey_analysis/correlation.py
@@ -1,29 +1,12 @@
 # Goal: given a csv file of survey responses, find the correlation 
 # that that demographic has to taking calc 2 in the future
 
-# Inputs:
-#   - The dataset to use
-#   - The given demographic: 
-#      - Select multiple options from:
-#          - Gender identity
-#          - Race/ethnicity
-#          - Highest math course
-#          - Field 
-#      - Default is all options
-
-# Simplify long names in survey:
-# Gender identity: [cis-woman, cis-man, trans-woman, trans-man, non-bin, agender, intersex, g-fluid, other]
-# Race/ethnicity: [white, black, indig-A-AK, asian, pac-isl, hisp-latx, 2+, other]
-# Highest math: [alg, pre-calc, calcAB, calcBC, other]
-# field: [bio, chem, phys, math, comp-sci, eng, econ, envs, bus, educ, perf-arts, vis_arts, soc-sci, eng-comm
-#         phil, mod-lan, med, ear-spc, law, soc-serv]
-
 import argparse
 import pandas as pd
 
 def main():
     parser = argparse.ArgumentParser(
-                description='Choose a search method to find a value in a list',
+                description='Find correlations for calculus 2 retention',
                 prog='correlation.py')
 
     parser.add_argument('--file_name',
@@ -52,8 +35,49 @@ def main():
     # Read in the survey data file
     data = pd.read_csv(args.file_name)
     
+    # Rename columns so they are easier to read and work with
+    data = data.set_axis(
+        ['gender_id', 'race_eth', 'hmath', 'field', 'calc2', 'reason'], 
+        axis=1)
+    
+    # Create dictionaries to shorten responses in the dataframe
+    gen_dict = {
+        '"Cis-gender woman (female at birth, identify as a woman)"': 'cis_W',      
+        '"Cis-gender man (male at birth, identify as a man)"': 'cis_M',
+        '"Transgender woman (male at birth, identify as a woman)"': 'trans_W',
+        '"Transgender man (female at birth, identify as a man)"': 'trans_M',
+        "Non-binary / genderqueer (don't identify exclusively as a man or a woman)": 'non_binary',
+        "Agender (don't identify as any gender)": 'agender',
+        'Intersex (have a combination of male and female biological traits)':'intersex',
+        'Gender fluid (gender identity and/or expression varies over time)': 'gender_fluid',
+        'Other:': 'other'}
+
+    race_dict = {
+        'White (non-Hispanic/Latinx)': 'white',
+         'Black/African American (non-Hispanic/Latinx)': 'black',
+         'Indigenous/Native American/Native Alaskan (non-Hispanic/Latinx)': 'native_amer_AK',
+         'Asian/Asian American (non-Hispanic/Latinx)': 'asian',
+         'Native Hawaiian/Pacific Islander (non-Hispanic/Latinx)': 'pacific_isl',
+         'Hispanic/Latinx': 'hisp_latx',
+         '2 or more races': '2+',
+         'Other:': 'other'}
+    
+    # Use dictionaries to shorten the responses
+    data['gender_id'] = data['gender_id'].map(gen_dict)
+    data['race_eth'] = data['race_eth'].map(race_dict)
+    
     # filter for just demographics we are interested in
-    #data_filtered = filter_data()
+    # First filter gender identity
+    filtered_gender = filter_data(
+      data, 
+      'gender_id', 
+      args.gender_identity)
+    
+    # Then filter race/ethnicity
+    filtered_gender_race = filter_data(
+       filtered_gender, 
+       'race_eth', 
+       args.race_ethnicity)
     
     # Find the cramers-v correlation coefficient
     #corr = cramersV(filtered_data)
