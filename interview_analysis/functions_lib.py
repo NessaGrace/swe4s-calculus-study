3c67e05c49302e2ad929b0feacf59627b81e8b65
Vanessa <vama3024@colorado.edu>
2022-10-16 20:09:50 -0600

    most recent version

--- interview_analysis/functions_lib.py
+++ interview_analysis/functions_lib.py
@@ -1,2 +1,7 @@
-def file_reader(file_name)
-    file
+def file_reader(file_name):
+    file_contents = open(file_name, 'r')
+    file_list = []
+    for line in file_contents:
+        file_list.append(line.rstrip().lower())
+    return file_list
+
