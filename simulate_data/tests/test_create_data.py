5356ee1b98347fb121893227c07ba48f351acfcc
marisapetrusky <marisa.petrusky@colorado.edu>
2022-10-20 16:45:11 +0000

    Unit tests for empty files

---
+++ simulate_data/tests/test_create_data.py
@@ -0,0 +1,36 @@
+import unittest
+import os
+import random
+import numpy as np
+import file_utils as util
+
+
+class TestCreateData(unittest.TestCase):
+    def setUp(self):
+        self.samp_size = 200
+
+        # Empty file for testing
+        self.empty_file = 'empty_file.txt'
+        f_empty = open(self.empty_file, 'w')
+        f_empty.close()
+
+        # Half filled file
+        self.half_file = 'half_file.txt'
+        f_half = open(self.half_file, 'w')
+        #for i in range(self.samp_size):
+            #f_empty.write('\t')
+        f_half.close()
+
+    def test_nonexist_file(self):
+        self.assertRaises(FileNotFoundError, util.read_data, 'fake_file.txt')
+
+    def test_empty_file(self):
+        self.assertRaises(TypeError, util.read_data, self.empty_file)
+
+    def tearDown(self):
+        os.remove(self.empty_file)
+
+
+if __name__ == '__main__':
+    unittest.main()
+        
