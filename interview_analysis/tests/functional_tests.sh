80d7e29c7af3573f895067256a7ffd478ec54160
Vanessa <vama3024@colorado.edu>
2022-10-24 10:22:50 -0600

    added functional tests for file_reader(), execution of deductive.py, needs bug fixes

--- interview_analysis/tests/functional_tests.sh
+++ interview_analysis/tests/functional_tests.sh
@@ -3,43 +3,21 @@
 test -e ssshtest || wget -q 'https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest'
 . ssshtest
 
-run linear_search python search.py --search_type linear
-assert_in_stdout 0
+run file_reader python ../read_files.py --file_name ../random_word_list.txt
+#assert_stdout
+assert_in_stdout "['two', 'red', 'magenta', 'pool', 'rent', 'orange', 'five', 'broadway', 'theater', 'theatre', 'joy', 'bro', 'chica', 'purple', 'work', 'lyric', 'ordinary', 'life', 'rock', 'in', 'the']"
+#assert_in_stdout ['two', 'red', 'magenta', 'pool', 'rent', 'orange', 'five', 'broadway', 'theater', 'theatre', 'joy', 'bro', 'chica', 'purple', 'work', 'lyric', 'ordinary', 'life', 'rock', 'in', 'the']
+#assert_in_stdout [two, red, magenta, pool, rent, orange, five, broadway, theater, theatre, joy, bro, chica, purple, work, lyric, ordinary, life, rock, in, the]
+#assert_in_stdout ['two', 'red', 'magenta', 'pool', 'rent', 'orange', 'five',\
+  #                'broadway', 'theater', 'theatre', 'joy', 'bro', 'chica',\
+  #                'purple', 'work', 'lyric', 'ordinary', 'life', 'rock',\
+   #               'in', 'the']
+
 assert_exit_code 0
 
-run binary_search python search.py --search_type binary
-assert_in_stdout 0
+run deductive python ../deductive.py \
+    --transcript_file_name ../sample_interview_text_with_questions.txt \
+    --word_list_file_name ../random_word_list.txt \
+    --questions_file_name ../sample_question_list.txt
+
 assert_exit_code 0
-
-
-run mean_threshold python search.py --search_type linear
-assert_in_stdout [[5, 6, 7, 8], [5, 6, 7, 8], [5, 6, 7, 8], [5, 6, 7, 8], \
-                  [5, 6, 7, 8], [5, 6, 7, 8], [5, 6, 7, 8], [5, 6, 7, 8], \
-                  [5, 6, 7, 8], [5, 6, 7, 8], [5, 6, 7, 8], [5, 6, 7, 8], \
-                  [5, 6, 7, 8], [5, 6, 7, 8], [5, 6, 7, 8], [5, 6, 7, 8], \
-                  [5, 6, 7, 8], [5, 6, 7, 8], [5, 6, 7, 8], [5, 6, 7, 8]] \
-                  ['Group 1', 'Group 3', 'Group 5', 'Group 7', 'Group 9', \
-                  'Group 11', 'Group 13', 'Group 15', 'Group 17', 'Group 19', \
-                  'Group 21', 'Group 23', 'Group 25', 'Group 27', 'Group 29', \
-                  'Group 31', 'Group 33', 'Group 35', 'Group 37', 'Group 39']
-assert_exit_code 0
-
-run plot_gtex python plot_gtex.py \
-    --gene_reads_file_name GTEx_Analysis_2017-06-05_v8_RNASeQCv1.1.9_gene_reads.acmg_59.gct.gz \
-    --sample_attributes_file_name GTEx_Analysis_v8_Annotations_SampleAttributesDS.txt \
-    --gene_name ACTA2 \
-    --output_file_name_all ACTA2.bs.png \
-    --output_file_name_high high_count.bs.png \
-    --mean_threshold 100000
-
-FILE=high_count.bs.png
-if test -f "$FILE"; then
-    echo "$FILE exists."
-fi
-
-FILE=ACTA2.bs.png
-if test -f "$FILE"; then
-    echo "$FILE exists."
-fi
-
-#assert_exit_code 0
