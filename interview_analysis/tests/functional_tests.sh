0e28257f52a7c273d34bcb9548d793af02696c00
Vanessa <vama3024@colorado.edu>
2022-10-30 14:20:15 -0600

    updated functional test with new file locations

--- interview_analysis/tests/functional_tests.sh
+++ interview_analysis/tests/functional_tests.sh
@@ -3,14 +3,14 @@
 test -e ssshtest || wget -q 'https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest'
 . ssshtest
 
-run file_reader python ../read_files.py --file_name ../random_word_list.txt
+run file_reader python ../read_files.py --file_name ../data_files/random_word_list.txt
 
 assert_in_stdout "['two', 'red', 'magenta', 'pool', 'rent', 'orange', 'five', 'broadway', 'theater', 'theatre', 'joy', 'bro', 'chica', 'purple', 'work', 'lyric', 'ordinary', 'life', 'rock', 'in', 'the']"
 assert_exit_code 0
 
 run deductive python ../deductive.py \
-    --transcript_file_name ../sample_interview_text_with_questions.txt \
-    --word_list_file_name ../random_word_list.txt \
-    --questions_file_name ../sample_question_list.txt
+    --transcript_file_name ../data_files/sample_interview_text_with_questions.txt \
+    --word_list_file_name ../data_files/random_word_list.txt \
+    --questions_file_name ../data_files/sample_question_list.txt
 
 assert_exit_code 0
