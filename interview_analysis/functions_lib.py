1bebafdd4f5972ffd91dc8c6678ab5bd840f4139
Vanessa <vama3024@colorado.edu>
2022-10-31 14:45:06 -0600

    added best practices

--- interview_analysis/functions_lib.py
+++ interview_analysis/functions_lib.py
@@ -1,4 +1,17 @@
 def file_reader(file_name):
+    """Read a text file and add contents to list by line
+
+    Parameters:
+    ----------
+    file_name : string
+                Name of the file to be processed
+
+    Returns:
+    --------
+    file_list : list of strings
+                List of text file contents by line
+   """
+
     file_contents = open(file_name, 'r')
     file_list = []
     for line in file_contents:
