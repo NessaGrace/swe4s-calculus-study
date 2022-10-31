52f2c013084c55115077187cace4ab44212da2fc
Vanessa <vama3024@colorado.edu>
2022-10-31 14:46:20 -0600

    cleaned up file and added best practices

--- interview_analysis/tests/functional_tests.sh
+++ interview_analysis/tests/functional_tests.sh
@@ -3,14 +3,15 @@
 test -e ssshtest || wget -q 'https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest'
 . ssshtest
 
-run file_reader python ../read_files.py --file_name ../data_files/random_word_list.txt
+# tests the function functions_lib.file_reader()
+run file_reader python ../read_files.py --file_name ../data_files/sample/random_word_list.txt
 
 assert_in_stdout "['two', 'red', 'magenta', 'pool', 'rent', 'orange', 'five', 'broadway', 'theater', 'theatre', 'joy', 'bro', 'chica', 'purple', 'work', 'lyric', 'ordinary', 'life', 'rock', 'in', 'the']"
 assert_exit_code 0
 
+# tests execution of the main script
 run deductive python ../deductive.py \
-    --transcript_file_name ../data_files/sample_interview_text_with_questions.txt \
-    --word_list_file_name ../data_files/random_word_list.txt \
-    --questions_file_name ../data_files/sample_question_list.txt
+    --word_list_file_name ../data_files/sim_word_list.txt \
+    --questions_file_name ../data_files/sim_ques.txt
 
 assert_exit_code 0
