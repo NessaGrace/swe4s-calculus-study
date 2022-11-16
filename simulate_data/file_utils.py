265c6701a621865495aca43c35e9d70875720565
marisapetrusky <marisa.petrusky@colorado.edu>
2022-11-16 18:52:47 +0000

    Weight chances of taking Calc II according to parameters file

--- simulate_data/file_utils.py
+++ simulate_data/file_utils.py
@@ -26,6 +26,7 @@ def read_data(file_name):
     question_header = []
     answer_header = []
     probability = []
+    correlation = []
     tolerance = 1e-3  # Tolerance for summation of probabilities
 
     # Go through parameter file, fill lists
@@ -35,7 +36,7 @@ def read_data(file_name):
         raise FileNotFoundError('Input parameter file name not found.')
 
     # Read questions, answers, and probabilities from parameter file
-    iter = 0
+    iter = 0  # Line in file
     if (os.path.getsize(file_name) > 0):
         for row in f_read:
             if (row):
@@ -46,9 +47,13 @@ def read_data(file_name):
 
             # If list does not contain first entry, must be blank
             if (info[0]):
-                if (iter % 2 == 0):
+                # Every third line contains question + answers
+                if (iter % 3 == 0):
                     question_header.append(info[0])
                     answer_header.append(info[1:])
+                # Every third - 2 line contains correlations
+                elif ((iter - 2) % 3 == 0):
+                    correlation.append(info[1:])
                 else:
                     probability.append(info[1:])
                 iter += 1
@@ -66,40 +71,59 @@ def read_data(file_name):
         sum = check_sum(probability[iQ])
         if abs(sum - 1) > tolerance:  # Sum = 1 within tolerance
             f_read.close()
+            pdb.set_trace()
             raise ArithmeticError('Warning: Probabilities of Question '
                                   + str(iQ) + ' does not ' + 'add to 1.')
 
     f_read.close()
 
-    return question_header, answer_header, probability
+    return question_header, answer_header, probability, correlation
 
 
 # Write simulated data to .csv file
-def write_data(file_name, question_header, answer_header, probability, size):
+def write_data(file_name, quest_head, ans_head, prob, corr, size):
+    takeC2_quest = ['Do you plan to take Calculus II?']
+    takeC2_ans = ['No', 'Maybe', 'Yes']
+    takeC2_prob = [0.3, 0.1, 0.6] # Can be fine tuned
+
     # Write simulated data to file
     with open(file_name, 'w', newline='') as f_write:
         writer = csv.writer(f_write)
-        writer.writerow(question_header)
-
+        writer.writerow(quest_head + takeC2_quest)
         for iSamp in range(0, size):  # Create int(size) simulated students
+            samp_weight = 1  # Combined weight of person taking Calculus II
             sample = []
             # For each question, randomly choose answer
-            for iQuest in range(0, len(question_header)):
+            for iQuest in range(0, len(quest_head)):
                 rand_no = random.random()  # Random number between 0 and 1 (Rf)
                 # Probability of having first answer is first probility (P1)
-                curr_prob = float(probability[iQuest][0])
+                curr_prob = float(prob[iQuest][0])
                 ans_idx = 0  # Index of current answer
                 not_accept = True  # Are we accepting answer?
                 while (not_accept):
                     # If Rf < P1, accept answer
                     if (rand_no < curr_prob):
                         not_accept = False
-                        sample.append(answer_header[iQuest][ans_idx])
+                        sample.append(ans_head[iQuest][ans_idx])
+                        samp_weight = samp_weight * float(corr[iQuest][ans_idx])
                     # If Rf > P1, reject answer
                     else:
                         ans_idx += 1
                         # By adding P1 + P2, we are checking if P1 < Rf < P2
-                        curr_prob += float(probability[iQuest][ans_idx])
+                        curr_prob += float(prob[iQuest][ans_idx])
+            # Now see if take Calc II with weight
+            rand_no = random.random()*samp_weight
+            curr_prob = float(takeC2_prob[0])
+            ans_idx = 0
+            not_accept = True
+            while (not_accept):
+                if (rand_no < curr_prob):
+                    not_accept = False
+                    sample.append(takeC2_ans[ans_idx])
+                else:
+                    ans_idx += 1
+                    curr_prob += takeC2_prob[ans_idx]
+
             writer.writerow(sample)
 
     return 0
