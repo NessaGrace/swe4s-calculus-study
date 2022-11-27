1ed0b734877bc60577a18cfbce33e718487c7f7e
Vanessa <vama3024@colorado.edu>
2022-11-27 12:01:14 -0700

    added working test for split_by_line (did test driven but forgot to commit), added failing test for sentence_splitter)

--- interview_analysis/tests/test_functions_lib.py
+++ interview_analysis/tests/test_functions_lib.py
@@ -30,9 +30,9 @@ class TestFunctionsLib(BaseTestCases.BaseTest):
         f = open(cls.test_file_name_lower, 'w')
 
         cls.test_file_list_lower = []
-        test_str_lower = ['apple', 'orange', 'strawberry', 'banana', 'kiwi']
+        cls.test_str_lower = ['apple', 'orange', 'strawberry', 'banana', 'kiwi']
 
-        for string in test_str_lower:
+        for string in cls.test_str_lower:
             cls.test_file_list_lower.append(string)
             f.write(string + '\n')
 
@@ -43,19 +43,27 @@ class TestFunctionsLib(BaseTestCases.BaseTest):
         g = open(cls.test_file_name_mixed, 'w')
 
         cls.test_file_list_mixed = []
-        test_str_mixed = ['APPLE', 'ORange', 'strawberry', 'banana',
+        cls.test_str_mixed = ['APPLE', 'ORange', 'strawberry', 'banana',
                           'kiwi']
 
-        for string in test_str_mixed:
+        for string in cls.test_str_mixed:
             cls.test_file_list_mixed.append(string.lower())
             g.write(string + '\n')
 
         g.close()
 
+        # prepare second list to filter out
+        cls.list_to_filter = ['apple', 'orange']
+
+        # prepare list of sentences
+        cls.sentence_list = ['I like pie.', 'I like cake.']
+
     @classmethod
     def tearDownClass(cls):
         os.remove(cls.test_file_name_lower)
         os.remove(cls.test_file_name_mixed)
+        cls.list_to_filter = None
+        cls.sentence_list = None
 
     # test file_reader() function
     def test_file_reader(self):
@@ -70,6 +78,40 @@ class TestFunctionsLib(BaseTestCases.BaseTest):
 
         # test if FileNotFoundError raised
         self.assertRaises(FileNotFoundError, fl.file_reader, "test_file.txt")
+        self.assertRaises(TypeError, fl.file_reader, 2)
+        self.assertRaises(TypeError, fl.file_reader, ["a", "b"])
+
+    # test filter_by_line() function
+    def test_filter_by_line(self):
+        filtered_list_fcn = fl.filter_by_line(self.test_str_lower,
+                                           self.list_to_filter)
+
+        filtered_list_test = ['strawberry', 'banana', 'kiwi']
+
+        # test if list filtered by function matches expected output
+        self.assertEqual(filtered_list_fcn, filtered_list_test)
+        # test if length of filtered list matches expected list length
+        self.assertEqual(len(filtered_list_fcn), len(filtered_list_test))
+        # test negative assertion
+        self.assertNotEqual(self.test_str_lower, filtered_list_fcn)
+        # test if length of filtered list less than length of original list
+        self.assertTrue(len(filtered_list_fcn) < len(self.test_str_lower))
+        self.assertFalse(len(filtered_list_fcn) > len(self.test_str_lower))
+        self.assertFalse(len(filtered_list_fcn) == len(self.test_str_lower))
+
+        # test error raising
+        self.assertRaises(TypeError, fl.filter_by_line, "a", ["a", "b"])
+        self.assertRaises(TypeError, fl.filter_by_line, 1, [1, 2])
+        self.assertRaises(TypeError, fl.filter_by_line, ["a", "b"], 2)
+
+    # test sentence_splitter() function
+    def test_sentence_splitter(self):
+        sentences_split_fcn = fl.sentence_splitter(self.sentence_list)
+
+        words_list = ['I', 'like', 'pie.', 'I', 'like', 'cake.']
+
+        # test if function successfully splits sentences into words
+        self.assertEqual(sentences_split_fcn, words_list)
 
 
 if __name__ == '__main__':
